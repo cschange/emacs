66a317434cda55f68b616862d562f0f0e5506018
cschange <17276903@qq.com>
2016-03-29 18:10:47 +0800

    Dtango0.02
    modify Dtango-theme.el

--- Dtango-theme.el
+++ Dtango-theme.el
@@ -34,28 +34,33 @@ Semantic, and Ansi-Color faces are included.")
 
 (let ((class '((class color) (min-colors 89)))
       ;; Tango palette colors.
-      (butter-1 "#FF6666") (butter-2 "#edd400") (butter-3 "#c4a000")
-      (orange-1 "#CCCCFF") (orange-2 "#f57900") (orange-3 "#ce5c00")
+      (func-1 "#CCFF66") (butter-2 "#edd400") (butter-3 "#c4a000")
+      (var-1  "#FFFFFF") (orange-2 "#f57900") (orange-3 "#ce5c00")
       
-      (choc-1 "#FFFFFF") (choc-2 "#c17d11") (choc-3 "#8f5902")
-      (cham-1 "#8ae234") (cham-2 "grey50") (cham-3 "#4e9a06") 
-      (blue-1 "#729fcf") (blue-2 "#3465a4") (blue-3 "#204a87") 
-      (plum-1 "#CCCCFF") (plum-2 "#a4a4fd") (plum-3 "#8b8bfb") ;;plum-1 #e090d7
-      (red-1 "#ef2929")  (red-2 "#cc0000")  (red-3 "#a40000")
-      (alum-1 "#eeeeec") (alum-2 "#d3d7cf") (alum-3 "#babdb6")
-      (alum-4 "#888a85") (alum-5 "#808080") (alum-6 "#2b303b")
+      (string-1 "#FFFFFF") (choc-2  "#c17d11") (choc-3  "#8f5902")
+      (cham-1   "#8ae234") (comment-1 "grey50") (cham-3 "#4e9a06") 
+      (blue-1   "#729fcf") (blue-2 "#3465a4")  (blue-3 "#204a87") 
+      (builtin-1 "#bf8dfb") (plum-2 "#a4a4fd") (plum-3 "#8b8bfb") ;;builtin-1 #e090d7
+      (red-1   "#ef2929")  (red-2  "#cc0000")  (red-3  "#a40000")
+      (alum-1  "#eeeeec")  (alum-2 "#d3d7cf")  (alum-3 "#babdb6")
+      (alum-4  "#888a85")  (alum-5 "#808080")  (alum-6 "#2b303b")
       ;; Not in Tango palette; used for better contrast.
-      (cham-0 "#b4fa70") (blue-0 "#99CCFF") (plum-0 "#FFCC99")
-      
-      (red-0 "#ff4b4b")  (alum-5.5 "#41423f") (alum-7 "#2b303b")
+      (keyword-1  "#f9067f")  (type-1   "#6666CC")  (cons-1   "#66CCFF")
+      (red-0      "#ff4b4b")  (alum-5.5 "#41423f")  (alum-7   "#2b303b")
+      (region-f   "#FFFFFF")  (region-b "#808080")  (cursor-f "#e9e9e9")
+      (pre-1      "#f9067f"))
       ;;custom by myself
-      ;;region : 二次选择  cursor-b 光标颜色 var-1 变量  type-1 类型
-      ;; string-1 字符串  keyword 关键字 func-1 函数 cons 常量域作用符
-      ;; builtin-1   comment-1 注释
-      (region-f   "#FFFFFF")    (region-b "#808080")  (cursor-f "#e9e9e9")
-      (var-1      "#FFFFFF")    (type-1   "#6666CC")  (string-1 "#FFFFFF")
-      (keyword-1  "#f9067f")    (func-1   "#CCFF66")  (cons-1   "#66CCFF")
-      (builtin-1  "#ccccff")    (comment-1 "grey50")  (pre-1    "#f9067f"))
+      ;; region :   二次选择  前景 ： (region-f   "#FFFFFF")  背景： (region-b "#808080")
+      ;; cursor-b   光标颜色       (cursor-f "#e9e9e9")
+      ;; var-1      变量           (var-1      "#FFFFFF")  
+      ;; type-1     类型           (type-1   "#6666CC")
+      ;; string-1   字符串         (string-1 "#FFFFFF")
+      ;; keyword-1  关键字         (keyword-1  "#f9067f")
+      ;; func-1     函数           (func-1   "#CCFF66") 
+      ;; cons       常量域作用符   (cons-1   "#66CCFF")
+      ;; builtin-1  BIF           (builtin-1  "#bf8dfb")
+      ;; comment-1  注释           (comment-1 "grey50")
+      
   ;;#FF9966
   (custom-theme-set-faces
    'Dtango
@@ -86,10 +91,10 @@ Semantic, and Ansi-Color faces are included.")
    `(compilation-mode-line-run  ((,class (:foreground ,orange-3))))
    `(compilation-mode-line-exit ((,class (:foreground ,cham-3))))
    ;; Escape and prompt faces
-   `(minibuffer-prompt ((,class (:foreground ,cham-0))))
+   `(minibuffer-prompt ((,class (:foreground ,keyword-1))))
    `(escape-glyph ((,class (:foreground ,butter-3))))
    `(error ((,class (:foreground ,red-0))))
-   `(warning ((,class (:foreground ,orange-1))))
+   `(warning ((,class (:foreground ,var-1))))
    `(success ((,class (:foreground ,cham-1))))
    ;; Font lock faces
    `(font-lock-builtin-face ((,class (:foreground ,builtin-1))))
@@ -115,23 +120,23 @@ Semantic, and Ansi-Color faces are included.")
    `(link ((,class (:underline t :foreground ,blue-1))))
    `(link-visited ((,class (:underline t :foreground ,blue-2))))
    ;; Gnus faces
-   `(gnus-group-news-1 ((,class (:foreground ,plum-1))))
+   `(gnus-group-news-1 ((,class (:foreground ,builtin-1))))
    `(gnus-group-news-1-low ((,class (:foreground ,plum-2))))
    `(gnus-group-news-2 ((,class (:foreground ,blue-1))))
    `(gnus-group-news-2-low ((,class (:foreground ,blue-2))))
    `(gnus-group-news-3 ((,class (:foreground ,cham-1))))
-   `(gnus-group-news-3-low ((,class (:foreground ,cham-2))))
-   `(gnus-group-news-4 ((,class (:foreground ,plum-0))))
+   `(gnus-group-news-3-low ((,class (:foreground ,comment-1))))
+   `(gnus-group-news-4 ((,class (:foreground ,cons-1))))
    `(gnus-group-news-4-low ((,class (:foreground ,choc-2))))
-   `(gnus-group-news-5 ((,class (:foreground ,orange-1))))
+   `(gnus-group-news-5 ((,class (:foreground ,var-1))))
    `(gnus-group-news-5-low ((,class (:foreground ,orange-2))))
    `(gnus-group-news-low ((,class (:foreground ,butter-2))))
-   `(gnus-group-mail-1 ((,class (:foreground ,plum-1))))
+   `(gnus-group-mail-1 ((,class (:foreground ,builtin-1))))
    `(gnus-group-mail-1-low ((,class (:foreground ,plum-2))))
    `(gnus-group-mail-2 ((,class (:foreground ,blue-1))))
    `(gnus-group-mail-2-low ((,class (:foreground ,blue-2))))
    `(gnus-group-mail-3 ((,class (:foreground ,cham-1))))
-   `(gnus-group-mail-3-low ((,class (:foreground ,cham-2))))
+   `(gnus-group-mail-3-low ((,class (:foreground ,comment-1))))
    `(gnus-group-mail-low ((,class (:foreground ,butter-2))))
    `(gnus-header-content ((,class (:weight normal :foreground ,butter-3))))
    `(gnus-header-from ((,class (:foreground ,butter-2))))
@@ -145,7 +150,7 @@ Semantic, and Ansi-Color faces are included.")
    `(message-header-subject ((,class (:foreground ,cham-1))))
    `(message-header-to ((,class (:foreground ,butter-2))))
    `(message-cited-text ((,class (:foreground ,cham-1))))
-   `(message-separator ((,class (:foreground ,plum-1))))
+   `(message-separator ((,class (:foreground ,builtin-1))))
    ;; SMerge faces
    `(smerge-refined-change ((,class (:background ,blue-3))))
    ;; Ediff faces
@@ -158,7 +163,7 @@ Semantic, and Ansi-Color faces are included.")
    `(ediff-even-diff-B ((,class (:background ,alum-5.5))))
    `(ediff-odd-diff-B ((,class (:background ,alum-5.5))))
    ;; Flyspell faces
-   `(flyspell-duplicate ((,class (:underline ,orange-1))))
+   `(flyspell-duplicate ((,class (:underline ,var-1))))
    `(flyspell-incorrect ((,class (:underline ,red-1))))
    ;; Semantic faces
    `(semantic-decoration-on-includes ((,class (:underline ,alum-4))))
@@ -175,8 +180,8 @@ Semantic, and Ansi-Color faces are included.")
 
   (custom-theme-set-variables
    'Dtango
-   `(ansi-color-names-vector [,alum-7 ,red-0 ,cham-0 ,butter-1
-			      ,blue-1 ,plum-1 ,blue-0 ,alum-1])))
+   `(ansi-color-names-vector [,alum-7 ,red-0 ,keyword-1 ,func-1
+			      ,blue-1 ,builtin-1 ,type-1 ,alum-1])))
 
 (provide-theme 'Dtango)
 
